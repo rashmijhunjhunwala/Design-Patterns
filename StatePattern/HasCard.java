0766c3d3fdf555046342a7494c178cd277142109
rashmijhunjhunwala <rashmijhunjhunwala068@gmail.com>
2020-02-04 00:45:08 +0530

    StatePattern

---
+++ StatePattern/HasCard.java
@@ -0,0 +1,46 @@
+
+public class HasCard implements ATMState{
+	ATMMachine atmMachine;
+	public HasCard(ATMMachine atmMachine)
+	{
+		this.atmMachine=atmMachine;
+	}
+	@Override
+public void insertCard() {
+		
+		System.out.println("You can only insert one card at a time");
+		
+	}
+
+	public void ejectCard() {
+		
+		System.out.println("Your card is ejected");
+		this.atmMachine.setATMState(atmMachine.getNoCardState());
+		
+	}
+
+	public void requestCash(int cashToWithdraw) {
+		
+		System.out.println("You have not entered your PIN");
+		
+		
+	}
+
+	public void insertPin(int pinEntered) {
+		
+		if(pinEntered == 1234){
+			
+			System.out.println("You entered the correct PIN");
+			this.atmMachine.correctPinEntered = true;
+			this.atmMachine.setATMState(atmMachine.getHasPin());
+			
+		} else {
+			
+			System.out.println("You entered the wrong PIN");
+			this.atmMachine.correctPinEntered = false;
+			System.out.println("Your card is ejected");
+			this.atmMachine.setATMState(atmMachine.getNoCardState());
+			
+		}	
+	}	
+}
